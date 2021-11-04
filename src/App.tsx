295cfe9467353cc445d3022da8105377aab0803f
Sunji <sophieadenn@gmail.com>
2021-11-04 11:13:09 +0900

    Local fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import React from "react";
+import { LoggedOutRouter } from "./routers/logged-out-router";
+
+const IS_LOGGED_IN = gql`
+  query isLoggedIn {
+    isLoggedIn @client
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { data } = useQuery(IS_LOGGED_IN);
+  console.log(data);
+  return <LoggedOutRouter />;
 }
 
 export default App;
