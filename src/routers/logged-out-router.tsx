608a8822434f5be3d2bbe23151973cc22211f904
Sunji <sophieadenn@gmail.com>
2021-11-08 15:10:10 +0900

    Router and @types

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,59 +1,20 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-
-// interface
-interface IForm {
-  email: string;
-  password: string;
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login } from "../pages/login";
+import { Signup } from "../pages/signup";
 
 export const LoggedOutRouter = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>();
-  const onSubmit = () => {
-    console.log(watch());
-  };
-  const onInvalid = () => {
-    console.log("Cant create accont");
-  };
-
   // 화면 구성
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register("email", {
-              required: "this is required.",
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="text-red-600">{errors.email?.message}</span>
-          )}
-          {errors.email?.type === "pattern" && (
-            <span className="text-red-600">Only Gmail Allowed</span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("password", { required: true })}
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-red-500">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
