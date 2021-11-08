0507e54f5b1c57a077420dc15203d03fdfbe67f8
Sunji <sophieadenn@gmail.com>
2021-11-08 23:44:24 +0900

    Tailwind css를 apply화

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,17 +3,11 @@ import React from "react";
 export const Login = () => {
   return (
     <span className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
-        <h3 className="text-3xl text-gray-800">Login</h3>
+      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center pt-5 pb-7">
+        <h3 className="text-3xl text-gray-800 font-bold">Log In</h3>
         <form className="flex flex-col mt-5 px-5">
-          <input
-            placeholder="Email"
-            className="bg-gray-200 shadow-inner focus:outline-none focus:border-green-500 border-2 focus:border-opacity-40 mb-3 py-3 px-4 rounded-3xl"
-          />
-          <input
-            placeholder="Password"
-            className="bg-gray-200 shadow-inner focus:outline-none focus:border-green-500 border-2 focus:border-opacity-40 py-3 px-4 rounded-3xl"
-          />
+          <input placeholder="Email" className="input mb-3" />
+          <input placeholder="Password" className="input" />
           <button className="px-5 py-3 bg-gray-800 text-white text-lg mt-3 rounded-xl hover:opacity-80">
             Log In
           </button>
