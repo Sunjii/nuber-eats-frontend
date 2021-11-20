cd44481e600c1cd6b7b7110ebb3cd1b808f12062
Sunji <sophieadenn@gmail.com>
2021-11-21 03:48:00 +0900

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NotFound = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h2 className="font-semibold text-4xl mb-3">Page Not Found.</h2>
+      <h4 className="font-medium text-lg mb-5">
+        찾는 페이지가 존재하지 않습니다.
+      </h4>
+      <Link className="hover:underline text-lime-600" to="/">
+        Go back home &rarr;
+      </Link>
+    </div>
+  );
+};
