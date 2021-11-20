693b5b1a598ee2ca6bcc8985092a0e7a2f03e28a
Sunji <sophieadenn@gmail.com>
2021-11-20 22:44:30 +0900

    me Query

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,34 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authToken = makeVar(token);
 
-export const client = new ApolloClient({
+// http link
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication toke from local
+  console.log(headers);
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authToken() || "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,9 +1,38 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import React from "react";
 import { isLoggedInVar } from "../apollo";
+import { meQuery } from "../__generated__/meQuery";
 
-export const LoggedInRouter = () => (
-  <div>
-    <h1>You are Logged In</h1>
-    <button onClick={() => isLoggedInVar(false)}>Log out</button>
-  </div>
-);
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+
+  // loading screen
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-bold text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+
+  // main screen
+  return (
+    <div>
+      <h1>You are Logged In!</h1>
+      <h2>{data.me.email}</h2>
+      <button onClick={() => isLoggedInVar(false)}>Log out</button>
+    </div>
+  );
+};
