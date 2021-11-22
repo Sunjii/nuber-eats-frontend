329bb570f894742443edb8b88f8a35fb4ea80925
Sunji <sophieadenn@gmail.com>
2021-11-22 23:11:58 +0900

    padding 설정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
   const { data } = useMe();
   return (
     <header className=" py-4">
-      <div className="w-full max-w-screen-xl mx-auto flex justify-between items-center">
+      <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
         <img src={nuberLogo} className="w-40" alt="" />
         <span className="text-xs">
           <Link to="/my-profile/">
