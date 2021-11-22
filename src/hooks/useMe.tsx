ba3c0b4ddff89f734df85e72c52846a4a1a92253
Sunji <sophieadenn@gmail.com>
2021-11-22 22:57:26 +0900

    useMe 훅을 사용하는 것의 장점

--- src/hooks/useMe.tsx
+++ src/hooks/useMe.tsx
@@ -13,6 +13,9 @@ const ME_QUERY = gql`
   }
 `;
 
+// queryHook을 사용하는 것으로 cache 효과를 얻을 수 있음. 여러번 호출하더라도 이미 데이터가 있다면 재호출 하지 않음.
+// deep component에게 props를 전달할때 효과적임
+
 export const useMe = () => {
   return useQuery<meQuery>(ME_QUERY);
 };
