3e893ec1d36106f0e29aa3b04389d1cbd7f6df35
Sunji <sophieadenn@gmail.com>
2021-11-26 02:35:16 +0900

    레스토랑 Query 작성 완료

---
+++ src/__generated__/restauarntsPageQuery.ts
@@ -0,0 +1,59 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { RestaurantsInput } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: restauarntsPageQuery
+// ====================================================
+
+export interface restauarntsPageQuery_allCategories_categories {
+  __typename: "Category";
+  id: number;
+  name: string;
+  coverImage: string | null;
+  slug: string;
+  restaurantCount: number;
+}
+
+export interface restauarntsPageQuery_allCategories {
+  __typename: "AllCategoriesOutput";
+  ok: boolean;
+  error: string | null;
+  categories: restauarntsPageQuery_allCategories_categories[] | null;
+}
+
+export interface restauarntsPageQuery_restaurants_results_category {
+  __typename: "Category";
+  name: string;
+}
+
+export interface restauarntsPageQuery_restaurants_results {
+  __typename: "Restaurant";
+  id: number;
+  name: string;
+  coverImage: string;
+  category: restauarntsPageQuery_restaurants_results_category | null;
+  address: string;
+  isPromoted: boolean;
+}
+
+export interface restauarntsPageQuery_restaurants {
+  __typename: "RestaurantsOutput";
+  ok: boolean;
+  error: string | null;
+  totalPages: number | null;
+  totalResults: number | null;
+  results: restauarntsPageQuery_restaurants_results[] | null;
+}
+
+export interface restauarntsPageQuery {
+  allCategories: restauarntsPageQuery_allCategories;
+  restaurants: restauarntsPageQuery_restaurants;
+}
+
+export interface restauarntsPageQueryVariables {
+  input?: RestaurantsInput | null;
+}
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,3 +1,37 @@
+import gql from "graphql-tag";
 import React from "react";
+
+const RESTAURANTS_QUERY = gql`
+  query restauarntsPageQuery($input: RestaurantsInput) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImage
+        slug
+        restaurantCount
+      }
+    }
+
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImage
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 
 export const Restaurants = () => <h1>Restauarnts</h1>;
