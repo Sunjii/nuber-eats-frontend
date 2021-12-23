56a0a97a3b2d95e70fed438019c712ec019646af
Sunji <sophieadenn@gmail.com>
2021-12-23 21:58:37 +0900

    useLocation과  useParam 비교

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,3 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
-export const Category = () => <h1>Category</h1>;
+export const Category = () => {
+  const location = useLocation();
+  const param = useParams();
+  useEffect(() => {
+    console.log(location);
+    console.log(param);
+  }, [location]);
+
+  return <h1>Cat</h1>;
+};
