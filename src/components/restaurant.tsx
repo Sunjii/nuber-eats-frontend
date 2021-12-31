cc5e2164c768d54a72e285a86ba8bda2cdfca2fb
Sunji <sophieadenn@gmail.com>
2021-12-31 22:33:12 +0900

    개별 레스토랑 페이지 구성

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface IRestaurantProps {
   id: string;
@@ -8,18 +9,21 @@ interface IRestaurantProps {
 }
 
 export const Restaurant: React.FC<IRestaurantProps> = ({
+  id,
   name,
   categoryName,
   coverImg,
 }) => (
-  <div>
-    <div
-      style={{ backgroundImage: `url(${coverImg})` }}
-      className="bg-red-500 py-28 bg-cover bg-center mb-3 max-w-xl"
-    ></div>
-    <h3 className="text-xl font-bold">{name}</h3>
-    <div className="border-t-2 mt-3 py-2 text-xs border-gray-300 opacity-50">
-      {categoryName}
+  <Link to={`/restaurants/${id}`}>
+    <div>
+      <div
+        style={{ backgroundImage: `url(${coverImg})` }}
+        className="bg-red-500 py-28 bg-cover bg-center mb-3 max-w-xl"
+      ></div>
+      <h3 className="text-xl font-bold">{name}</h3>
+      <div className="border-t-2 mt-3 py-2 text-xs border-gray-300 opacity-50">
+        {categoryName}
+      </div>
     </div>
-  </div>
+  </Link>
 );
